1adae209a3618577e5fc41d725033d42d5c5cf01
Shaq <shobbsdaley@gmail.com>
2019-02-28 09:54:54 +0000

    Cleaned up my error handlers

--- app.js
+++ app.js
@@ -1,52 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const apiRouter = require('./routes/apiRouter');
+const {
+  handleStatus, handle400, handle422, handle500, handleBadUrl,
+} = require('./errors');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/api', apiRouter);
 
-app.use('/*', (req, res, next) => {
-  res.status(404).json({ msg: 'Looks like you have put in an incorrect address' });
-});
+app.use('/*', handleBadUrl);
 
-app.use((err, req, res, next) => {
-  if (err.status) {
-    res.status(err.status).json({ msg: err.msg });
-  } else next(err);
-});
-
-app.use((err, req, res, next) => {
-  if (err.code === '42703') {
-    res.status(400).json({ msg: 'No matching columns' });
-  } else next(err);
-});
-
-app.use((err, req, res, next) => {
-  if (err.code === '23502') {
-    res.status(400).json({ msg: 'There is data missing in the body for this post' });
-  } else next(err);
-});
-
-app.use((err, req, res, next) => {
-  if (err.code === '23503') {
-    res
-      .status(400)
-      .json({ msg: 'Something doesnt exist, either the topic or the username or the article' });
-  } else next(err);
-});
-
-app.use((err, req, res, next) => {
-  if (err.code === '23505') {
-    // console.log(err); The console.log would log the error
-    const msg = err.detail;
-    res.status(422).json({ msg });
-  }
-});
-
-app.use((err, req, res, next) => {
-  res.status(500).json({ msg: '......it appears we have a little problem here', err });
-});
+app.use(handleStatus);
+app.use(handle400);
+app.use(handle422);
+app.use(handle500);
 
 module.exports = app;
--- errors/index.js
+++ errors/index.js
@@ -1,3 +1,36 @@
 exports.handle405 = (req, res, next) => {
   res.status(405).json({ msg: `The ${req.method} method is not allowed at this endpoint` });
 };
+
+exports.handleStatus = (err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).json({ msg: err.msg });
+  } else next(err);
+};
+
+exports.handle400 = (err, req, res, next) => {
+  if (err.code === '42703') {
+    res.status(400).json({ msg: 'No matching columns' });
+  } else if (err.code === '23502') {
+    res.status(400).json({ msg: 'There is data missing in the body for this post' });
+  } else if (err.code === '23503') {
+    res
+      .status(400)
+      .json({ msg: 'Something doesnt exist, either the topic or the username or the article' });
+  } else next(err);
+};
+
+exports.handle422 = (err, req, res, next) => {
+  if (err.code === '23505') {
+    const msg = err.detail;
+    res.status(422).json({ msg });
+  }
+};
+
+exports.handle500 = (err, req, res, next) => {
+  res.status(500).json({ msg: '......it appears we have a little problem here', err });
+};
+
+exports.handleBadUrl = (req, res, next) => {
+  res.status(404).json({ msg: 'Looks like you have put in an incorrect address' });
+};
