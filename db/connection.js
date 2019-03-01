7b08c1a7953cc1623dc82007bd9e08f3af373e61
Shaq <shobbsdaley@gmail.com>
2019-03-01 15:06:07 +0000

    Heroku config

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,8 @@
 const ENV = process.env.NODE_ENV || 'development';
-const knex = require('knex');
 const dbConfig = ENV === 'production'
   ? { client: 'pg', connection: process.env.DATABASE_URL }
-  : require('../knexfile');
+  : require('../knexfile.js');
 
-const connection = knex(dbConfig);
+const connection = require.("knex")(dbConfig);
 
 module.exports = connection;
